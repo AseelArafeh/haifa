9b99c4d016b4bbbfd2a44ec934866792405d7294
AseelArafeh <arafehaseel@gmail.com>
2020-09-19 10:58:43 +0300

    init commit

---
+++ src/main/java/edu/ppu/services/ListMaxService.java
@@ -0,0 +1,9 @@
+package edu.ppu.services;
+
+import java.util.List;
+
+public class ListMaxService {
+    public int getMax(List<Integer> numbers) {
+        throw new RuntimeException("this method is not implemented yet");
+    }
+}
