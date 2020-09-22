c2a4e0cf18376964bf4a4ab854e4ccb754887ee1
latifaIssa <latifa.masri1998@gmail.com>
2020-09-22 07:31:46 +0300

    HP-3-adding dedenceies

---
+++ src/test/java/edu/ppu/services/ListSumServiceTest.java
@@ -0,0 +1,12 @@
+package edu.ppu.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListSumServiceTest {
+
+    @Test
+    void sum() {
+    }
+}
