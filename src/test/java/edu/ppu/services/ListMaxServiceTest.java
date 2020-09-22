39ebdfcd97088df47f7b0b06ba5f1e5687dc2e83
deema1999 <hafezdema1999@gmail.com>
2020-09-22 07:00:47 +0300

    HP-2:prepare test for max service

---
+++ src/test/java/edu/ppu/services/ListMaxServiceTest.java
@@ -0,0 +1,19 @@
+package edu.ppu.services;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class ListMaxServiceTest {
+
+        @Test
+        public void testMax(){
+
+            List<Integer> list = Arrays.asList(5, 2, 10);
+            ListMaxService listMax = new ListMaxService();
+            int res = listMax.getMax(list);
+            assertEquals(10, res);
+        }
+
+}
