c039b5ef2c0c571420c2371cfcff3351b2b997bc
deema1999 <hafezdema1999@gmail.com>
2020-09-26 06:18:41 +0300

    HP-4:add imports to test file

--- src/test/java/edu/ppu/services/SumOverMaxServiceTest.java
+++ src/test/java/edu/ppu/services/SumOverMaxServiceTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import java.util.*;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SumOverMaxServiceTest {
 
     @Test(expected = RuntimeException.class)
     public void testSumOverMaxEmptyArray() {
         SumOverMaxService sumOverMax = new SumOverMaxService(null, null);
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<Integer>();
         float rest = sumOverMax.getSumOverMax(list);
 
     }
@@ -22,7 +24,7 @@ public class SumOverMaxServiceTest {
         SumOverMaxService sumOverMax = new SumOverMaxService(mockMAX, mockSUM);
         List<Integer> list = Arrays.asList(5, 2, 10);
         when(mockMAX.getMax(list)).thenReturn(10);
-        when(mockSUM.getSUM(list)).thenReturn(17);
+        when(mockSUM.getSum(list)).thenReturn(17);
         float res = sumOverMax.getSumOverMax(list);
         assertEquals(1.7, res);
     }
